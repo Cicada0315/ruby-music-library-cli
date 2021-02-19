b8d6c7d8e8362edacfda8b20be14b022f9d83cfd
jean <jin.ahn.1027@gmail.com>
2021-02-19 15:29:26 -0500

    commit tree fd35177eed985169615bfabe7cc86ddb3460e3ac

---
+++ lib/artist.rb
@@ -0,0 +1,43 @@
+class Artist
+  extend Concerns::Findable
+  attr_accessor :name
+  @@all=[]
+  def initialize(name)
+    @name=name
+    @songs=[]
+    #save
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end 
+
+  def save
+    @@all<<self
+  end
+ 
+  def self.create(name)
+    #Artist.new(name).save
+    artist=Artist.new(name)
+    artist.save
+    artist
+  end
+
+  def songs
+    @songs
+  end
+
+  def add_song(song)
+    song.artist = self if song.artist == nil
+    @songs << song unless @songs.include?(song)
+    song
+  end
+
+  def genres
+    songs.collect {|song| song.genre}.uniq
+  end
+end
